b444d363e9650b1c77e1eb85ab0ab728cc677886
kahina-git <77074219+kahina-git@users.noreply.github.com>
2021-09-03 09:28:07 -0400

    Ajiout d'un System out

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,8 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bonjour Git...!");
+        System.out.println("remier commit...");
     }
     
 }
